89688977ba98d4acdc764a1e5eddb4313d9bd932
netra-patwari <patwarinetra@gmail.com>
2024-03-10 05:56:33 +0530

    saas configurationadded

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
+import path from 'path'
+// const path = require('path')
+
+// const nextConfig = {
+//     sassOptions: {
+//         includePaths: [path.join(__dirname, 'styles')],
+//       },
+// }
 
 const path = require('path')
-
-const nextConfig = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-      },
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
-
 
 module.exports = nextConfig
